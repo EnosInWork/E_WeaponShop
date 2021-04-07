5982bc11f9856fa482cdd44f5847c517831482c0
Five-Dev <haxy60@gmail.com>
2021-04-07 20:42:13 +0200

    Up

---
+++ client.lua
@@ -0,0 +1,199 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(100)
+	end
+end)
+
+Citizen.CreateThread(function()
+	for k, v in pairs(ConfAmmu.Pos) do
+		local blip = AddBlipForCoord(v.x, v.y, v.z)
+
+		SetBlipSprite(blip, 110)
+		SetBlipScale (blip, 0.75)
+		SetBlipColour(blip, 3)
+		SetBlipAsShortRange(blip, true)
+
+		BeginTextCommandSetBlipName('STRING')
+		AddTextComponentSubstringPlayerName('Armurerie')
+		EndTextCommandSetBlipName(blip)
+	end
+end)
+
+Citizen.CreateThread(function()
+    local hash = GetHashKey("s_m_y_ammucity_01")
+    while not HasModelLoaded(hash) do
+    RequestModel(hash)
+    Wait(20)
+	end
+	for k,v in pairs(ConfAmmu.AmmuPed) do
+	ped = CreatePed("PED_TYPE_CIVMALE", "s_m_y_ammucity_01", v.x, v.y, v.z, v.a, false, true)
+	SetBlockingOfNonTemporaryEvents(ped, true)
+	FreezeEntityPosition(ped, true)
+    SetEntityInvincible(ped, true)
+	end
+end)
+
+ppa = false
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		local playerCoords = GetEntityCoords(PlayerPedId())
+
+		for k, v in pairs(ConfAmmu.Pos) do
+			local distance = GetDistanceBetweenCoords(playerCoords, v.x, v.y, v.z, true)
+
+            if distance < 10.0 then
+                actualZone = v
+
+                zoneDistance = GetDistanceBetweenCoords(playerCoords, actualZone.x, actualZone.y, actualZone.z, true)
+
+				DrawMarker(21, v.x, v.y, v.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.4, 10, 10, 255, 100, false, true, 2, false, nil, nil, false)
+            end
+            
+            if distance <= 1.5 then
+				RageUI.Text({
+					message = "Appuyez sur [~b~E~w~] pour ouvrir l'armurerie",
+					time_display = 1
+				})
+
+                if IsControlJustPressed(1, 51) then
+                    RageUI.Visible(RMenu:Get('enos_ammu', 'main'), not RageUI.Visible(RMenu:Get('enos_ammu', 'main')))
+                end
+            end
+
+            if zoneDistance ~= nil then
+                if zoneDistance > 1.5 then
+                    RageUI.CloseAll()
+                end
+            end
+        end
+    
+	end
+end)
+
+RMenu.Add('enos_ammu', 'main', RageUI.CreateMenu("Ammunation", "Armurerie"))
+RMenu.Add('enos_ammu', 'armurerie', RageUI.CreateSubMenu(RMenu:Get('enos_ammu', 'main'), "Armurerie", "Voici nos armes blanche."))
+RMenu.Add('enos_ammu', 'arme', RageUI.CreateSubMenu(RMenu:Get('enos_ammu', 'main'), "Armurerie", "Voici nos armes à feu."))
+RMenu.Add('enos_ammu', 'aces', RageUI.CreateSubMenu(RMenu:Get('enos_ammu', 'main'), "Armurerie", "Voici nos accessoires."))
+RMenu.Add('enos_ammu', 'ppa', RageUI.CreateSubMenu(RMenu:Get('enos_ammu', 'main'), "Armurerie", "Confirmation"))
+
+Citizen.CreateThread(function()
+    while true do
+        RageUI.IsVisible(RMenu:Get('enos_ammu', 'main'), true, true, true, function()
+
+            RageUI.ButtonWithStyle("Armes blanche", "Pour accéder aux armes blanche.", {RightLabel = "→"},true, function()
+            end, RMenu:Get('enos_ammu', 'armurerie'))
+
+			ESX.TriggerServerCallback('e_weaponshop:checkLicense', function(cb)            
+                if cb then
+                    ppa = true 
+                	else 
+                 	ppa = false   
+                end
+              end)
+
+              if ppa then 
+              RageUI.ButtonWithStyle("Armes à feu", "Pour accéder aux armes à feu.", { RightLabel = "→" }, true, function(Hovered, Active, Selected)     
+                if (Selected) then
+              
+                end
+              end, RMenu:Get('enos_ammu', 'arme')) 
+              
+              else 
+
+                RageUI.ButtonWithStyle("Armes à feu", "Pour accéder aux armes à feu.", { RightBadge = RageUI.BadgeStyle.Lock }, false, function(Hovered, Active, Selected)     
+                    if (Selected) then
+                    end
+                  end) 
+
+                end
+
+			RageUI.ButtonWithStyle("Accessoires", "Pour accéder aux accessoires.", {RightLabel = "→"},true, function()
+            end, RMenu:Get('enos_ammu', 'aces')) 
+
+            if ppa then
+
+                RageUI.ButtonWithStyle("Acheter le P.P.A", "Vous avez le PPA.", { RightBadge = RageUI.BadgeStyle.Lock }, false, function(Hovered, Active, Selected)     
+                    if (Selected) then
+                    end
+                  end)
+
+                else 
+
+			RageUI.ButtonWithStyle("Acheter le P.P.A", "Permet de débloquer les armes à feu", {RightLabel = "~g~50,000$"},true, function()
+            end, RMenu:Get('enos_ammu', 'ppa'))
+
+            
+        end
+
+
+        end, function()
+        end)
+
+		RageUI.IsVisible(RMenu:Get('enos_ammu', 'ppa'), true, true, true, function()
+
+                RageUI.ButtonWithStyle("Confirmer", nil, { }, true, function(Hovered, Active, Selected)
+                    if Selected then
+						local prix = 50000
+						TriggerServerEvent('Eweapon:addppa', 'weapon')
+						RageUI.GoBack()
+                    end
+                end)
+
+				RageUI.ButtonWithStyle("~r~Annuler", nil, { }, true, function(Hovered, Active, Selected)
+                    if Selected then
+						RageUI.GoBack()
+                    end
+                end)
+
+        end, function()
+        end)
+
+        RageUI.IsVisible(RMenu:Get('enos_ammu', 'armurerie'), true, true, true, function()
+
+            for k, v in pairs(ConfAmmu.Type.Blanche) do
+                RageUI.ButtonWithStyle(v.Label, nil, {RightLabel = "~g~"..v.Price.."$"}, true, function(Hovered, Active, Selected)
+                    if Selected then
+                        TriggerServerEvent('weaponshops:giveWeapon', v)
+                    end
+                end)
+            end
+
+        end, function()
+        end)
+
+		RageUI.IsVisible(RMenu:Get('enos_ammu', 'arme'), true, true, true, function()
+
+            for k, v in pairs(ConfAmmu.Type.Armes) do
+                RageUI.ButtonWithStyle(v.Label, nil, {RightLabel = "~g~"..v.Price.."$"}, true, function(Hovered, Active, Selected)
+                    if Selected then
+                        TriggerServerEvent('weaponshops:giveWeapon', v)
+                    end
+                end)
+            end
+
+        end, function()
+        end)
+
+        RageUI.IsVisible(RMenu:Get('enos_ammu', 'aces'), true, true, true, function()
+
+            for k, v in pairs(ConfAmmu.Type.Items) do
+                RageUI.ButtonWithStyle(v.Label, nil, {RightLabel = "~g~"..v.Price.."$"}, true, function(Hovered, Active, Selected)
+                    if Selected then
+                        TriggerServerEvent('item:acheter', v)
+                    end
+                end)
+            end
+			
+        end, function()
+        end)
+
+        Citizen.Wait(0)
+    end
+end)
+
+
---
+++ config.lua
@@ -0,0 +1,44 @@
+ConfAmmu = {}
+
+ConfAmmu = {
+    Pos = {
+		{x = -3171.70, y = 1087.66, z = 19.83},
+		{x = 2567.6, y = 294.3, z = 108.7},
+		{x = 22.0, y = -1107.2, z = 29.8},
+		{x = 252.3, y = -50.0, z = 69.9},
+		{x = -330.2, y = 6083.8, z  =31.4},
+		{x = 1693.4, y = 3759.5, z = 34.7},
+		{x = -662.1, y = -935.3, z = 21.8}
+	},
+	
+    AmmuPed = {
+		{x = 23.17, y = -1105.32, z = 28.8, a = 160.2},
+		{x = -3173.81, y = 1088.71, z = 19.83, a = 246.79},
+		{x = -331.92, y = 6085.38, z = 30.45, a = 223.21},
+		{x = 1692.01, y = 3761.31, z = 33.70, a = 228.42},
+		{x = 2567.77, y = 292.12, z = 107.73, a = 357.27},
+		{x = 254.20, y = -50.88, z = 68.94, a = 72.29},
+		{x = -662.15, y = -933.52, z = 20.82, a = 181.72}
+	},
+
+    Type = {
+
+		Blanche = {
+			{Label = 'Lampe Torche', Value = 'WEAPON_FLASHLIGHT', Price = 90},
+			{Label = 'Poing américain', Value = 'WEAPON_KNUCKLE', Price = 120},
+			{Label = 'Couteau', Value = 'WEAPON_KNIFE', Price = 150},
+			{Label = 'Batte de baseball', Value = 'WEAPON_BAT', Price = 200}
+		},
+
+		Armes = {
+			{Label = 'Pétoire', Value = 'WEAPON_SNSPISTOL', Price = 15000},
+			{Label = 'Tazer', Value = 'WEAPON_STUNGUN', Price = 500}
+		},
+
+		Items = {
+			{Label = 'Chargeur', Value = 'clip', Price = 250},
+			{Label = 'Jumelle', Value = 'jumelles', Price = 300},
+			{Label = 'Kevlar', Value = 'kevlar', Price = 500}
+		}
+	}
+}
